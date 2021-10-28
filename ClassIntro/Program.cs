12d1ce0636bb3c305954f168388f15698b1d6720
ozmen <ozmen@DESKTOP-OTFBAI3>
2021-10-29 01:42:32 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Berk Özmenekşe";
+            kurs2.IzlenmeOrani = 94;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Beril Özmenekşe";
+            kurs3.IzlenmeOrani = 99;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Aysen Özmenekşe";
+            kurs4.IzlenmeOrani = 99;
+
+            //Console.WriteLine("Kurs'un Adı" + " : "  + kurs1.KursAdi + " " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4};
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine("Kurs'un Adı" +  " : "  + kurs.KursAdi + "  " + "Kursu Veren Eğitmen" + " : " + kurs.Egitmen);
+            }
+
+            //Console.WriteLine();
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi  { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            // type safety = tip güvenliği veri tipi tanımlarken onun veri tipini yazmak zorundasın.
+            //do not repeat yourself = kendini tekrarlama
+            //kategoriEtiketi bir değer tutucu yani alias
+            //yönetim dedi ki artık kategori değil kategoriler olsun..
+            //ondalıklı sayılar double veri tipinde gösteriyoruz.
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 9.50;
+            double dolarBugun = 9.60;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Olunuz");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+            
+        }
+    }
+}
